564e7f08c51e534c14c1cc98c7b9b9abd513d93f
Skullmasher <4488386+SkullMasher@users.noreply.github.com>
2021-05-07 03:30:14 +0200

    fix: :skull: switch to unoptimized variable font of heebo

--- components/HeaderCallToAction.vue
+++ components/HeaderCallToAction.vue
@@ -3,7 +3,7 @@
     <div>
       <a
         href="#"
-        class="inline-block mb-5 py-4 px-7 bg-blue-500 rounded-lg uppercase font-bold"
+        class="inline-block mb-5 py-4 px-7 bg-blue-500 rounded-lg uppercase font-bold tracking-wider"
       >
         Consulter les services
       </a>
@@ -11,7 +11,7 @@
     <div>
       <a
         href="#"
-        class="inline-block py-4 px-7 border-2 border-blue-500 rounded-lg uppercase font-bold"
+        class="inline-block py-4 px-7 border-2 border-blue-500 rounded-lg uppercase font-bold tracking-wider"
       >
         Voir le portfolio
       </a>
--- components/HeaderTagline.vue
+++ components/HeaderTagline.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="mb-14">
-    <h2 class="mb-4 text-xl">Florian Ledru</h2>
-    <h1 class="text-4xl leading-relaxed">
-      Informaticien <strong>freelance</strong> au service des
-      <strong>PME</strong>
+    <h2 class="mb-4 text-2xl">Florian Ledru</h2>
+    <h1 class="text-4xl font-thin leading-relaxed">
+      Informaticien <strong class="font-bold">freelance</strong> au service des
+      <strong class="font-bold">PME</strong>
     </h1>
   </div>
 </template>
