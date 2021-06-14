2495ba31296d0f2e6410fc80f842a1be837137cf
Skullmasher <4488386+SkullMasher@users.noreply.github.com>
2021-06-14 03:35:06 +0200

    feat: :skull: a la cama

--- components/header/HeaderHeroImage.vue
+++ components/header/HeaderHeroImage.vue
@@ -1,7 +1,8 @@
 <template>
   <nuxt-picture
     src="/img/florian-skullmasher-ledru.jpg"
-    class="hero-image"
+    class="hero-image shadow-lg transition-all"
+    sizes="lg:100vw xl:45vw"
   />
 </template>
 
@@ -10,8 +11,20 @@
   clip-path: polygon(0 0, 100% 0, 100% 82%, 0% 100%);
 }
 
+@media (min-width: 1025px) {
+  .hero-image {
+    clip-path: polygon(0 0, 60% 0, 69% 66%, 0% 100%);
+  }
+}
+
 /* max-width does not fill the container entirely on desktop  */
 .hero-image img {
   /* width: 100%; */
+}
+
+@media (min-width: 1025px) {
+  .hero-image img {
+    /* border-radius: 50%; */
+  }
 }
 </style>
--- components/header/HeaderLogo.vue
+++ components/header/HeaderLogo.vue
@@ -1,8 +1,19 @@
 <template>
   <nuxtLink
     to="/"
-    class="p-1 bg-gray-200 rounded-full transform transform-gpu rotate-12 hover:-rotate-12 hover:scale-105 hover:ring-4 ring-gray-200 ring-opacity-30 transition-all shadow-lg"
+    class="p-1 bg-gray-200 rounded-full transform transform-gpu rotate-12 hover:-rotate-12 hover:scale-105 hover:ring-4 ring-gray-200 ring-opacity-30 transition-all shadow-lg relative"
   >
     <img src="~assets/skullmasher.svg" class="w-10 h-10 sm:w-20 sm:h-20" />
   </nuxtLink>
 </template>
+<style>
+.logo-halo::before {
+  content: '';
+  position: absolute;
+  background-image: radial-gradient(hsla(194, 76%, 54%), rgba(0, 0, 0, 0) 50%);
+  top: -10%;
+  left: -20px;
+  width: 100px;
+  height: 100px;
+}
+</style>
