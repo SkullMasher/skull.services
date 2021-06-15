c0e5310c6e6e031badae91c9a9c9a8e41c58ce66
Skullmasher <4488386+SkullMasher@users.noreply.github.com>
2021-06-15 12:08:08 +0200

    feat: :skull: Add cookie notice & contraint to about

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -1,6 +1,12 @@
 <template>
-  <footer class="flow-root bg-indigo-200 dark:bg-gray-800 pt-5 pb-5 pl-5 pr-5">
+  <footer class="flow-root bg-indigo-200 dark:bg-gray-800 pt-5 pb-5 md:pt-10 md:pb-10 pl-5 pr-5">
     <p class="mb-5">Numéro siren : 793553488</p>
-    <a href="https://github.com/SkullMasher/skull.services">code source</a>
+    <a
+      href="https://github.com/SkullMasher/skull.services"
+      class="block mb-3 underline"
+    >
+      code source
+    </a>
+    <p>Ce site n'utilise pas de cookie, car il fais un régime keto.</p>
   </footer>
 </template>
--- components/main/MainShortAboutMe.vue
+++ components/main/MainShortAboutMe.vue
@@ -1,26 +1,28 @@
 <template>
-  <div class="md:flex mx-5 mb-20 text-lg">
-    <figure class="mb-10">
-      <nuxt-picture
-        src="/img/florian-skullmasher-ledru-kek.jpg"
-        class="about-image"
-      />
-      <figcaption class="w-32 mx-auto text-sm text-center italic">
-        Florian au parc naturel des Alpilles en juin 2021
-      </figcaption>
-    </figure>
-    <div class="">
-      <h2 class="mb-6 text-3xl">Geek à plein temps&nbsp;!</h2>
-      <p class="ml-5 mb-10">
-        Actuelement, j'opère exclusivement en ligne puisque je suis <a href="">situé en campagne d'Arles</a>.
-        Issu de la banlieue parisienne, je peux me déplacer dans les villes de
-        Paris, Nantes, Toulouse, Montpellier, Nimes, Saragosse ou Madrid.
-      </p>
+  <div>
+    <div class="max-w-4xl lg:mx-auto md:flex mx-5 mb-20 text-lg">
+      <figure class="mb-10 max-w-xs mx-auto flex-grow">
+        <nuxt-picture
+          src="/img/florian-skullmasher-ledru-kek.jpg"
+          class="about-image"
+        />
+        <figcaption class="w-32 mx-auto text-sm text-center italic">
+          Florian au parc naturel des Alpilles en juin 2021
+        </figcaption>
+      </figure>
+      <div class="flex-shrink md:ml-10 md:mr-5">
+        <h2 class="mb-6 text-3xl">Geek à plein temps&nbsp;!</h2>
+        <p class="ml-5 mb-10">
+          Actuelement, j'opère exclusivement en ligne puisque je suis <a href="">situé en campagne d'Arles</a>.
+          Issu de la banlieue parisienne, je peux me déplacer dans les villes de
+          Paris, Nantes, Toulouse, Montpellier, Nimes, Saragosse ou Madrid.
+        </p>
+      </div>
     </div>
   </div>
 </template>
 <style>
 .about-image img {
-  @apply shadow-lg ring-4 rounded-full mb-5 hover:ring-8 ring-elegiac-light hover:ring-elegiac dark:ring-indigo-100 hover:dark:ring-indigo-200 ring-opacity-60 hover:ring-opacity-100 transition-all transform transform-gpu hover:-rotate-2;
+  @apply shadow-lg ring-4 rounded-full mb-5 hover:ring-8 ring-elegiac-light hover:ring-elegiac dark:ring-gray-100 hover:dark:ring-indigo-200 ring-opacity-80 hover:ring-opacity-100 transition-all transform transform-gpu hover:-rotate-2;
 }
 </style>
