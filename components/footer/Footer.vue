068142069fad2e5eeb29e32c7dca4fd517dbcad3
Skullmasher <4488386+SkullMasher@users.noreply.github.com>
2021-06-22 05:03:25 +0200

    feat: :skull: BORN

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -1,49 +1,54 @@
 <template>
-  <footer class="flow-root bg-indigo-200 dark:bg-gray-800 pt-5 pb-5 md:pt-10 md:pb-10 pl-5 pr-5">
-    <div class="max-w-3xl mx-auto space-y-5">
-      <p class="text-center uppercase">Numéro siren : 793553488</p>
-      <p v-show="$colorMode.value === 'light'">
-        Deviens un vrai hacker et appuie sur la lune pour activer le
-        <em class="text-black">dark mode</em>.
-        <button @click="setDarkMode()">
-          <nuxt-img
-            class="w-8 md:w-10 ml-2 inline-block cursor-pointer transform transition duration-500 hover:rotate-45 opacity-75 hover:opacity-100 moon-shine"
-            format="svg"
-            src="/img/moon.svg"
-          />
-        </button>
-      </p>
-      <p v-show="$colorMode.value === 'dark'">
-        Appuie sur le soleil pour activer le
-        <em class="text-white">light mode</em>
-        et t'exploser les yeux !
-        <button @click="setLightMode()">
-          <nuxt-img
-            class="w-8 md:w-10 ml-2 inline-block cursor-pointer transform transition duration-500 hover:-rotate-180 opacity-75 hover:opacity-100 sun-shine"
-            format="svg"
-            src="/img/sun.svg"
-          />
-        </button>
-      </p>
-      <p>
-        Si vous regarder
-        <a
-          href="https://github.com/SkullMasher/skull.services"
-          class="link-pretty"
-        >
-          le code source de ce site
-        </a>
-        vous remarquez que je n'ai pas utilisé wordpress.
-      </p>
-      <p>Ce site n'utilise pas de cookie &#127850;, car il fais un régime keto &#129361;.</p>
-      <p>Normalement ici je met une citation d'un rocker mort à 27 ans.</p>
+  <footer class="flow-root bg-indigo-200 dark:bg-gray-800 pt-10 pb-10 md:pt-10 md:pb-10 pl-5 pr-5">
+    <p class="text-center uppercase mb-10">Numéro siren : 793553488</p>
+    <div class="lg:flex max-w-screen-lg mx-auto">
+      <div class="max-w-3xl mx-auto mb-10 space-y-5">
+        <p v-show="$colorMode.value === 'light'">
+          Deviens un vrai hacker et appuie sur la lune pour activer le
+          <em class="text-black">dark mode</em>.
+          <button @click="setDarkMode()">
+            <nuxt-img
+              class="w-8 md:w-10 ml-2 inline-block cursor-pointer transform transition duration-500 hover:rotate-45 opacity-75 hover:opacity-100 moon-shine"
+              format="svg"
+              src="/img/moon.svg"
+            />
+          </button>
+        </p>
+        <p v-show="$colorMode.value === 'dark'">
+          Appuie sur le soleil pour activer le
+          <em class="text-white">light mode</em>
+          et t'exploser les yeux !
+          <button @click="setLightMode()">
+            <nuxt-img
+              class="w-8 md:w-10 ml-2 inline-block cursor-pointer transform transition duration-500 hover:-rotate-180 opacity-75 hover:opacity-100 sun-shine"
+              format="svg"
+              src="/img/sun.svg"
+            />
+          </button>
+        </p>
+        <p>
+          Si vous regarder
+          <a
+            href="https://github.com/SkullMasher/skull.services"
+            class="link-pretty"
+          >
+            le code source de ce site
+          </a>
+          vous remarquez que je n'ai pas utilisé wordpress.
+        </p>
+        <p>Ce site n'utilise pas de cookie &#127850;, car il fais un régime keto &#129361;.</p>
+        <p>Normalement ici je met une citation d'un rocker mort à 27 ans.</p>
+      </div>
+      <hl2Born class="w-28 mx-auto fill-current text-gray-700 transition duration-1000 hover:text-yellow-500" />
     </div>
   </footer>
 </template>
 <script>
 import Vue from 'vue'
+import Hl2Born from "~/static/img/hl2-born.svg?inline";
 
 export default Vue.extend({
+  components: { Hl2Born },
   methods: {
     setLightMode() {
       this.$colorMode.preference = 'light'
