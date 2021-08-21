9b1f7af225f64109128677e18242ef5933db3b2b
Skullmasher <4488386+SkullMasher@users.noreply.github.com>
2021-08-21 16:15:13 +0200

    fix: :skull: eliminate layout shifting in header for phones & tablet

--- components/header/HeaderHeroImage.vue
+++ components/header/HeaderHeroImage.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <figure class="hero-image">
+    <figure class="lg:hidden clip-header-image h-48 sm:h-96">
       <nuxt-picture
         src="/florian-skullmasher-ledru.jpg"
         sizes="xs:100vw sm:100vw md:100vw"
+        class="hero-img"
       />
     </figure>
     <nuxt-picture
       src="/florian-skullmasher-ledru-square.jpg"
-      class="hero-image-desktop"
+      class="hero-img-desktop"
       sizes="lg:320px xl:384px"
     />
   </div>
