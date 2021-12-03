2886d04e44db952b0c9b2ab77c286c2483e920b0
Skullmasher <4488386+SkullMasher@users.noreply.github.com>
2021-12-03 01:49:52 +0100

    Add: :skull: Content to service page

--- pages/services.vue
+++ pages/services.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flow-root bg-indigo-100 text-gray-800 dark:bg-elegiac dark:text-gray-200 transition duration-500">
     <Header />
-    <main class="max-w-sm mx-5 sm:mx-auto my-10">
-      <section>
+    <main>
+      <section class="max-w-sm mx-5 sm:mx-auto my-10">
         <p class="text-2xl sm:text-4xl mb-5">Page en construction</p>
         <NuxtLink
           to="/"
@@ -11,8 +11,43 @@
           Retour à l'accueil
         </NuxtLink>
       </section>
-      <section class="hidden">
-        <h2>Wordpress</h2>
+      <section class="max-w-4xl lg:mx-auto mx-5 mb-20 text-lg">
+        <div class="mb-20">
+          <h2 class="text-2xl sm:text-4xl mb-5">Accélérer mon site Wordpress</h2>
+          <div class="sm:grid grid-cols-3 gap-4">
+            <div class=""></div>
+            <div class="col-span-2">
+              <p class="mb-5">Mon site est lent et j'ai besoin de l'optimiser rapidement pour améliorer mon score Google Page Speed.</p>
+              <ul class="ml-10 mb-5 list-disc space-y-1">
+                <li>0 plugins payant, pas de paiement mensuel</li>
+                <li>Optimisation automatique des images</li>
+                <li>Mise en cache</li>
+                <li>Minification des HTML, CSS et Javascript</li>
+                <li>Chargement diféré des script et images.</li>
+              </ul>
+              <p class="text-4xl mb-5">150 €</p>
+              <button class="btn">Contacter maintenant</button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 class="text-2xl sm:text-4xl mb-5">Intégration responsive de maquette</h2>
+          <div class="sm:grid grid-cols-3 gap-4">
+            <div class=""></div>
+            <div class="col-span-2">
+              <p class="mb-5">Intégration responsive mobile first de maquettes pour tous type de framework.</p>
+              <ul class="ml-10 mb-5 list-disc space-y-1">
+                <li>Wordpress</li>
+                <li>React / Next.JS</li>
+                <li>Vue / NuxtJS</li>
+                <li>Animation CSS</li>
+              </ul>
+              <p class="text-4xl mb-5">À partir 300 €</p>
+              <button class="btn">Demander mon devis</button>
+            </div>
+          </div>
+        </div>
+
       </section>
     </main>
     <Footer />
