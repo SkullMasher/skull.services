09c3fb816d06e66bbaaa1c2ac65085044be56f7f
Skullmasher <4488386+SkullMasher@users.noreply.github.com>
2021-12-04 14:19:39 +0100

    Add: :skull: Form might be secure who knows

--- api/formchecker.js
+++ api/formchecker.js
@@ -1,10 +1,31 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import nodemailer from 'nodemailer'
+import rateLimit from 'express-rate-limit'
+import slowDown from 'express-slow-down'
+import xss from 'xss-clean'
+import helmet from 'helmet'
 
 const app = express()
 app.use(bodyParser.json())
 app.use(express.json({ limit: '10kb' }))
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 50 // limit each IP to 50 requests per windowMs
+})
+app.use(limiter)
+
+const speedLimiter = slowDown({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  delayAfter: 15, // allow 25 requests per 15 minutes, then...
+  delayMs: 1000 // begin adding 1000ms of delay per request
+})
+app.use(speedLimiter)
+
+app.use(helmet())
+
+app.use(xss())
 
 let transporter = nodemailer.createTransport({
   host: "heartlessgaming.com",
