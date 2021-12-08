6c55bc7f3171077b6fcde51098bbe167e7c7630a
Skullmasher <4488386+SkullMasher@users.noreply.github.com>
2021-12-08 01:17:46 +0100

    Fix: :skull: Typoz

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -52,13 +52,13 @@
       </section>
       <MainProject
         name="Auzars"
-        title="Auzars - Vendre en ligne les produits d'une entreprise local"
+        title="Auzars - Vendre en ligne les produits d'une entreprise locale"
         :tags="['wordpress', 'e-commerce', 'référencement SEO', 'design UI/UX']"
         logo="/logo-auzars-site-noir.webp"
         url="https://www.auzars.com/"
       >
-        <p>Transformation du thème original Wordpress pour simplifier l'achat et la personalisation des produits. Présentation clair des options disponible pour changer le produit en fonction de sa dimension ou du cadre choisis par l'utilisateur.</p>
-        <p>Depuis le début de notre colaboration, l'ensemble des mesures prise ont permis de réussir la transition d'une entreprise local vers la vente en ligne, qui représente maintenant une partie du chifre d'affaire mensuel.</p>
+        <p>Transformation du thème original Wordpress pour simplifier l'achat et la personnalisation des produits. Présentation claire des options disponibles pour changer le produit en fonction de sa dimension ou du cadre choisi par l'utilisateur.</p>
+        <p>Depuis le début de notre collaboration, l'ensemble des mesures prises ont permis de réussir la transition d'une entreprise locale vers la vente en ligne, qui représente maintenant une partie du chiffre d'affaire mensuel.</p>
       </MainProject>
       <MainProject
         name="Infinity Graphic"
@@ -69,9 +69,9 @@
         logoHeight="103"
         url="https://infinitygraphic.fr/"
       >
-        <p>Mon profil compléte parfaitement les compétences de l'agence pour corriger les bugs les plus tenaces et améliorer la sécurité des site.</p>
-        <p>Mes compétences varié du design jusqu'au serveur me permettent d'intervenir rapidement sur toute la chaine qui constitue les problèmes de votre agence web avec ses projets informatique.</p>
-        <p>Venant également d'une agence web, je peux guider votre agence à faire les bon choix technologique en fonction des besoins et rester rentables dans vos coups de dévelopement.</p>
+        <p>Mon profil complète parfaitement les compétences de l'agence pour corriger les bugs les plus tenaces et améliorer la sécurité des sites.</p>
+        <p>Mes compétences variées, du design jusqu'au serveur me permettent d'intervenir rapidement sur toute la chaîne qui constitue les problèmes de votre agence web avec ses projets informatiques.</p>
+        <p>Venant également d'une agence web, je peux guider votre agence à faire les bon choix technologiques en fonction des besoins et rester rentable dans vos coûts de développement.</p>
       </MainProject>
       <MainProject
         name="Heartless Gaming"
@@ -82,22 +82,22 @@
         logoHeight="104"
         url="https://heartlessgaming.com/"
       >
-        <p>Fatigué de ne pas trouver des serveurs de jeux et un espace de communication, un groupe de gamer ce décide de se lancer dans l'aventure de l'auto hébergement.</p>
-        <p>Heartless Gaming est mon projet de coeur. A travers ce projet j'ai appris l'admnistration système, la gestion d'une commnauté en ligne.</p>
-        <p>Toute les compétences apprise sur ce projet sont mise à profit au travers de ma société.</p>
-        <p>Ce site web prend en charge les donations à travers stripe, affiche le nombre de joueur en ligne grace <a href="https://github.com/heartless-gaming/heartlessgaming.com">un système complétement maison</a>.</p>
+        <p>Fatigué de ne pas trouver de serveurs de jeux et un espace de communication, un groupe de gamers décide de se lancer dans l'aventure de l'auto-hébergement.</p>
+        <p>Heartless Gaming est mon projet de coeur. A travers ce projet j'ai appris l'administration système, la gestion d'une communauté en ligne.</p>
+        <p>Toutes les compétences apprises sur ce projet sont mises à profit au travers de ma société.</p>
+        <p>Ce site web prend en charge les donations à travers <a href="https://stripe.com" class="link-pretty" target="_blank">Stripe</a>, affiche le nombre de joueurs en ligne grace <a href="https://github.com/heartless-gaming/heartlessgaming.com">un système complètement maison</a>.</p>
       </MainProject>
       <MainProject
         name="Seb Services Auto"
-        title="Seb Services Auto - Démarer sa société avec la bonne visibilité"
+        title="Seb Services Auto - Démarrer sa société avec la bonne visibilité"
         :tags="['design UI/UX', 'wordpress', 'référencement SEO']"
         logo="/logo-seb-services-auto.png"
         logoWidth="300"
         logoHeight="104"
         url="https://www.sebservicesauto.fr/"
       >
-        <p>Site vitrine complet assurant une très bonne présance en ligne, face à la concurance. Peu de visite mais un fort très fort taux de conversion sous la forme d'apell téléphonique, grace à un référencement localisé sur la zone d'activité de l'entreprise.</p>
-        <p>Le site web de seb et son référencement sur Google Maps lui apporte un flu constant de nouveaux client, très rapidement fidélisé et heureux de trouver une alternative aux garages automobile classique.</p>
+        <p>Site vitrine complet assurant une très bonne présence en ligne, face à la concurrence. peu de visites mais un très fort taux de conversion sous la forme d'appels téléphoniques, grace à un référencement localisé sur la zone d'activité de l'entreprise.</p>
+        <p>Le site web de Seb et son référencement sur Google Maps lui apporte un flux constant de nouveaux clients très rapidement fidélisés et heureux de trouver une alternative aux garages automobiles classiques.</p>
       </MainProject>
       <section id="contact" class="group pt-20 pb-20 text-lg bg-indigo-200 dark:bg-elegiac-dark clippath-contactbloc">
         <div class="mx-5">
