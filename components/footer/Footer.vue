def16b021c7f5c8b96488b8713e4e6277b9588fe
Skullmasher Heartless <4488386+SkullMasher@users.noreply.github.com>
2023-05-15 04:55:07 +0200

    feat: it's unicorn time !

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -1,9 +1,9 @@
 <template>
-  <footer class="flow-root bg-gray-200 dark:bg-gray-800 pt-10 pb-10 md:pt-10 md:pb-10 pl-5 pr-5 transition duration-500">
+  <footer class="flow-root bg-gray-200 dark:bg-gray-800 pt-10 md:pt-10 transition duration-500 overflow-hidden">
     <p class="text-center uppercase mb-10">
       Numéro siren : 793553488
     </p>
-    <div class="lg:flex max-w-screen-lg mx-auto">
+    <div class="lg:flex max-w-screen-lg mx-auto mb-20 px-5">
       <div class="max-w-3xl mx-auto">
         <div class="flex gap-3 items-center mb-3">
           <div>
@@ -29,8 +29,34 @@
           </a>,
           vous remarquerez que je n'ai pas utilisé Wordpress.
         </p>
-        <p>Ce site n'utilise pas de cookie &#127850;, car il fait un régime keto &#129361;.</p>
+        <p class="mb-4">Ce site n'utilise pas de cookie &#127850;, car il fait un régime keto &#129361;.</p>
+        <p class="mb-4">J'ai pas de réseaux sociaux mais parfois <NuxtLink to="https://youtu.be/nF40HTXmYtQ" target="_blank" class="link-pretty">je fais des vidéos sur internet</NuxtLink>.</p>
+        <p class="">On m'a demandé de mettre une licorne içi ¯\_(ツ)_/¯</p>
+      </div>
+    </div>
+    <div class="unicorn-gradient">
+      <div class="animate-unicorn">
+        <nuxt-picture src="/img/unicorn.png" width="300" height="217" loading="lazy"></nuxt-picture>
       </div>
     </div>
   </footer>
 </template>
+
+<style>
+.unicorn-gradient {
+  background: radial-gradient(80% 85% at 50% 100%, hsla(266, 60%, 70%, 1) 0%, rgba(255, 255, 255, 0) 100%);
+}
+
+.animate-unicorn {
+  animation: unicorn 5s infinite ease-in-out;
+}
+
+@keyframes unicorn {
+    from {
+        transform: translateX(-300px);
+    }
+    to {
+        transform: translateX(100%);
+    }
+}
+</style>
