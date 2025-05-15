1f4261edf67b7a35dfb789015410df75919a4057
Skullmasher <4488386+SkullMasher@users.noreply.github.com>
2025-05-15 11:03:37 +0200

    feat: SEO title & description

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Florian Ledru - Informaticien freelance au service des PME',
+  ogTitle: 'Florian Ledru - Informaticien freelance au service des PME',
+  description: 'Votre entreprise a besoin de visibilités pour vendre sur internet. Je réalise des sites web et applications simple et ergonomique pour vous accompagner.',
+  ogDescription: 'Votre entreprise a besoin de visibilités pour vendre sur internet. Je réalise des sites web et applications simple et ergonomique pour vous accompagner.',
+})
+</script>
+
 <template>
   <div>
     <NuxtLayout>
