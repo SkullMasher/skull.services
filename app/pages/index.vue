4c6c91dfda7429acea7a3a197f32ec77c91d6df1
Skullmasher Heartless <4488386+SkullMasher@users.noreply.github.com>
2026-04-21 21:10:46 +0200

    feat: hero header image tryouts

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,15 +2,17 @@
   <div>
     <Navbar />
     <div class="my-8">
-      <header class="mx-auto px-2 mb-12 max-w-xl">
-        <h2 class="text-2xl mb-4">
+      <header class="relative mx-auto px-2 pb-86 max-w-xl border-b-primary-content">
+        <h1 class="text-4xl mb-4 font-semibold">
+          Réduire les coûts informatiques de votre
+          <abbr class="text-primary" title="Petites et moyennes entreprises">PME</abbr>
+        </h1>
+        <h2 class="text-2xl mb-2">
           Florian Ledru
         </h2>
-        <h1 class="text-4xl">
-          Informaticien <span class="text-secondary">freelance</span> au service
-          des
-          <abbr class="text-secondary" title="Petites et moyennes entreprises">PME</abbr>
-        </h1>
+        <h2>Développement • Hébergement • Design</h2>
+        <NuxtPicture src="/florian-ledru-dev-2026.avif" format="avif, webp" width="300"
+          alt="Florian Ledru détouré avec la main sur le sur le matin" class="absolute bottom-0 right-0 -z-10" />
       </header>
       <main class="mx-auto px-2 max-w-3xl">
         <ContactForm />
